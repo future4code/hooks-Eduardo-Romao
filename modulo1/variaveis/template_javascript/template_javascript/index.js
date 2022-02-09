ed0cf8253710b6bd2ee58d996c5627c545639284
Edaurdo-js-gif/aula-git-git-hooks <edu.romao.and@gmail.com>
2022-02-08 22:39:53 -0300

    Enviando o primeiro execício de interpretação do código

---
+++ modulo1/variaveis/template_javascript/template_javascript/index.js
@@ -0,0 +1,24 @@
+/*    1- Analise o programa abaixo e diga o que será impresso
+      no console?
+Resposta :
+10
+10 5 
+
+      2- Analise o programa abaixo e diga o que será impresso no
+      console?
+Resposta: 
+10 10 10 
+     
+      3- Analise o programa abaixo, entenda o que ele faz e suria 
+      melhores nomes para as variáveis. Lembre-se que devemos 
+      escolher nomes significativos, usar o padrão camelCase. Além
+      disso, os nomes não podem começar com números ou caracteres
+      especiais.
+Resposta: 
+let p = prompt("quantasHorasVoceTrabaha");
+let t = prompt("quantoVoceRecebePorDia");
+alert("voceRecebe")
+
+*/
+
+
