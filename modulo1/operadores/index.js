bec57163b68f1a7359da8c68efe7b3bbc46cb4ba
Edaurdo-js-gif/aula-git-git-hooks <edu.romao.and@gmail.com>
2022-02-15 20:10:45 -0300

    Enviando o exercício de operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,64 @@
+
+//Exercícios de interpretação de código.
+
+//Exercício 1
+// a) false
+// b) false
+// c) true 
+// d) boleano
+
+//Exercício 2
+/*
+let  primeiroNumero = Number (prompt("Digite um segundo número!"));
+let segundoNumero = Number( prompt("digite o segundo numero"));
+const soma =  primeiroNumero + segundoNumero;
+console.log(soma);
+
+
+//Exercício 3
+Deve-se converter as strings primeiroNumero e segundoNumero
+em Number.
+
+//Exercícios de escrita de código.
+
+// 1- 
+let idadeDoUsuario = Number (prompt("Qual é a sua idade?"));
+let idadeMelhor = Number (prompt("Qual é a idade do seu melhor amigo?"));
+const resultado = idadeDoUsuario > idadeMelhor; 
+const resultado1 = idadeDoUsuario % idadeMelhor;
+console.log(resultado);
+console.log(resultado1);
+
+// 2-
+let insiraPar = Number (prompt("Insira um número par:"));
+let numerodivisivel = 2
+let resultado2 = insiraPar - numerodivisivel;
+let resultado3 = insiraPar % numerodivisivel;
+console.log(resultado2);
+console.log(resultado3); 
+
+// 3-
+let idadeDoUsuario = Number (prompt("Qual é a sua idade?"));
+let meses = idadeDoUsuario * 12;
+let dias = idadeDoUsuario * 365; 
+let horas = 24 * 365;
+console.log("Meses:" , meses); 
+console.log("Dias:" , dias); 
+console.log("Horas:" , horas); 
+
+// 4-
+let numero1 = Number (prompt("Digite um número"));
+let numero2 = Number (prompt("Digite um número"));
+const resposta1 = numero1 > numero2;
+const resposta2 = numero1 === numero2;
+const resposta3 = numero1 / 1;
+const resposta4 = numero2 / numero1;
+console.log(resposta1);
+console.log(resposta2);
+console.log(resposta3);
+console.log(resposta4);
+*/
+
+
+console.log('Alfabeto'.toUpperCase());
+console.log('ALFABETO'.toLowerCase());
